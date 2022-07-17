6626f315515cfa84bbb0f9dfc18d8b4ba5f6ca1d
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2022-07-17 20:37:30 +0700

    fix: change pop up menu to fix bugs

--- travel/js/main.js
+++ travel/js/main.js
@@ -1,7 +1,7 @@
-// Burger handler
+// Burger menu
 
-const navBurger = document.querySelector('.header__nav-close-button');
-const headerNav = document.querySelector('.header__nav')
+/* const navBurger = document.querySelector('.header__nav-close-button');
+const headerNav = document.querySelector('.header__nav');
 
 function active() {
   navBurger.classList.toggle('header__nav-active');
@@ -10,43 +10,70 @@ function active() {
 
 document.addEventListener('click', (event => {
   if (event.target.classList.contains('header__nav-close-button')) {
-      active();
+    active();
   }
   else if (!event.target.classList.contains('header__nav') &&
-      headerNav.classList.contains('header__nav-active')) {
-      active();
+    headerNav.classList.contains('header__nav-active')) {
+    active();
   }
-}))
+}))*/
 
-/*(function () {
-  const burger= document.querySelector('.burger');
-  const menu = document.querySelector('.header__nav');
-  const menuClose = document.querySelector('.header__nav-close');
-  const menuLinks = document.querySelectorAll('.header__link');
+// Pop up
 
-  burger.addEventListener('click', () => {
-    menu.classList.add('header__nav-active');
+const loginWindow = {
+  title: "Login to your account",
+  button: "Sign In",
+  account: "Don't have an account?",
+  link: "Register"
+}
+const createWindow = {
+  title: "Create account",
+  button: "Sign Up",
+  account: "Already have an account?",
+  link: "Log in"
+}
+const headerButton = document.querySelector('.header__login-form')
+const accountButton = document.querySelector('.header__item-account')
+const popupActive = 'login__popup-active';
+const popup = document.querySelector('.login__pop-up');
+const popupClick = document.querySelector('.login__register-url');
+const popupTitle = document.querySelector('.login__caption');
+const popupButton = document.querySelector('.login__sign-button-text');
+const popupText = document.querySelector('.login__register-question');
+const loginButton = document.querySelector('.login__sign-button');
 
-  });
+function ChangePopUpWindow(event) {
+  let setOfWords = (event.target.innerHTML != "Register")?loginWindow:createWindow;
+  popup.classList.toggle(popupActive);
+  popupTitle.innerHTML = setOfWords.title;
+  popupButton.innerHTML = setOfWords.button;
+  popupText.innerHTML = setOfWords.account;
+  popupClick.innerHTML = setOfWords.link;
+}
 
-  menuClose.addEventListener('click', () => {
-    menu.classList.remove('header__nav-active');
-  });
+function makePopUpVisible(element){
+  element.stopPropagation();
+  popup.classList.toggle('login__pop-up-visible');
+  shadow.classList.add('body__shadow-active');
+}
 
-  if (window.innerWidth <= 390) {
-    for (let i = 0; i < menuLinks.length; i += 1) {
-      menuLinks[i].addEventListener('click', () => {
-        menu.classList.remove('header__nav_active');
-      });
+function makePopUpInvisible(event) {
+  let target = event.target;
+  let popupIsUp = popup == target || popup.contains(target);
+  if (popup.classList.contains('login__pop-up-visible') && !popupIsUp) {
+      popup.classList.remove('login__pop-up-visible');
+      shadow.classList.remove('body__shadow-active');
     }
-  }
+}
 
-  function closeBurger() {
-    menu.classList.remove('header__nav-active');
-  }
+function alertLoginPassword() {
+  let loginValue = document.getElementById('login').value;
+  let passwordValue = document.getElementById('password').value;
+  alert(`Login: ${loginValue}\nPassword: ${passwordValue}`);
+}
 
-  document.body.addEventListener("click", function(event) {
-    if ((event.target != menu)) closeBurger();
-  });
-
-}());*/
+popupClick.addEventListener('click', ChangePopUpWindow);
+headerButton.addEventListener('click', makePopUpVisible);
+accountButton.addEventListener('click', makePopUpVisible);
+loginButton.addEventListener('click', alertLoginPassword);
+document.addEventListener('click', makePopUpInvisible);
